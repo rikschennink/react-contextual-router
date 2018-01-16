436892e6cd112709e761d083eae86a1e2f81baee
Rik Schennink <rikschennink@gmail.com>
2018-01-16 08:55:20 +0100

    restructured files

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as conditioner from 'conditioner-core';
 
 const hasQuery = child => !!child.props.query;
 const isFallback = child => !hasQuery(child);
 
+// <Context query="@media (min-width:30em)"></Context>
+// <Context query="@media (min-width:30em)" component={MyComponent}/>
+// <Context>Fallback content</Context>
+export class Context extends Component {
+    render() {
+        const { component, children } = this.props;
+
+        if (component) return React.createElement(component);
+
+        if (children) return this.props.children;
+
+        return null;
+    }
+}
+
+// PropsTypes
+Context.propTypes = {
+    query: PropTypes.string,
+    component: PropTypes.func
+};
+
 // <ContextRouter/>
 export class ContextRouter extends Component {
-    state = {
-        matches: []
-    };
+    constructor(props) {
+        super(props);
+        this.state = { matches: [] };
+    }
 
     componentDidMount() {
         const { children } = this.props;
